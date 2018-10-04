9643c1ac3e8b6b9af1acf20e8426b57de58c1461
ryan-devs <43006434+ryan-devs@users.noreply.github.com>
2018-10-04 12:23:09 -0500

    created simple sum calculator in js
    for the first js section exercises

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let first_num = prompt("Welcome to calculator. Enter the first value:");
+
+let second_num = prompt("Welcome to calculator. Enter the second value:");
+
+let sum = Number(first_num) + Number(second_num);
+
+alert("The sum of " + first_num + " + " + second_num + " = " + sum);
